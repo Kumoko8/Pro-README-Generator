d02eddd84de296c11b472b517e72f565ea9d4aba
Kumoko8 <131223690+Kumoko8@users.noreply.github.com>
2023-07-11 12:37:13 -0500

    Create generateMarkdown.js

---
+++ generateMarkdown.js
@@ -0,0 +1,30 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license)
+  const licenseLink = renderLicenseLink(data.license)
+  const licenseSection = renderLicenseSection(data.license)
+  return `# ${data.title} ${licenseBadge} ${licenseLink} ${licenseSection}
+`;
+//use this space to get the format of the ReadMe before you run it
+
+}
+
+module.exports = generateMarkdown;
